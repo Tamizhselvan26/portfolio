348f22f9ef338540240fe05c5231406e213aa754
TAMIZH SELVAN R <108929260+Tamizhselvan26@users.noreply.github.com>
2026-01-21 18:04:08 +0530

    Update resume download link with versioning

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,7 +7,8 @@ export default function Resume(){
         <div  className='md:w-1/2 flex justify-center'>
             <div className='flex flex-col justify-center text-white'>
             <h1 className='text-4xl border-b-4 border-[rgb(115,168,206)] mb-5 w-[140px] font-bold'>Resume</h1>
-            <p className='pb-5' >You can view my resume <a className='btn' href='TAMIZHSELVAN R RESUME.pdf' download="TAMIZHSELVAN R RESUME.pdf">Download</a></p>
+            <p className='pb-5' >You can view my resume <a className='btn' href='TAMIZHSELVAN R RESUME.pdf?v=2' download="TAMIZHSELVAN R RESUME.pdf">Download</a>
+</p>
         </div>
             
         </div>
